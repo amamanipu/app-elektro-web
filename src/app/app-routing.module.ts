98bd7ce2c78c4190b79a250dd3c740918f262b6d
Nicolas Hidalgo <nicolashidalgocorrea@hotmail.com>
2021-11-12 14:06:40 -0500

    cambiosNico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { ChangePasswordComponent } from './access/change-password/change-passwor
 import { LoginComponent } from './access/login/login.component';
 import { ProductComponent } from './access/product/product.component';
 import { RegisterComponent } from './access/register/register.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { HomeComponent } from './home/home.component';
+import { PagoComponent } from './pago/pago.component';
+import { TiendaComponent } from './tienda/tienda.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'changepassword', component: ChangePasswordComponent },
   { path: 'product', component: ProductComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'tienda', component: TiendaComponent },
+  { path: 'carrito', component: CarritoComponent },
+  { path: 'pago', component: PagoComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './access/login/login.component';
 import { RegisterComponent } from './access/register/register.component';
+
 import { ChangePasswordComponent } from './access/change-password/change-password.component';
 import { ProductComponent } from './access/product/product.component';
+
+import { HomeComponent } from './home/home.component';
+import { TiendaComponent } from './tienda/tienda.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { PagoComponent } from './pago/pago.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +21,11 @@ import { ProductComponent } from './access/product/product.component';
     LoginComponent,
     RegisterComponent,
     ChangePasswordComponent,
-    ProductComponent
+    ProductComponent,
+    HomeComponent,
+    TiendaComponent,
+    CarritoComponent,
+    PagoComponent
   ],
   imports: [
     BrowserModule,
